797427d03b8e40770ebed2c2c99cf6d7710d51e0
Katharina Hammerschmidt <khammers@1-C-17.42heilbronn.de>
2022-08-11 01:01:26 +0200

    ex03

--- CPP06/ex02/Base.cpp
+++ CPP06/ex02/Base.cpp
@@ -1,6 +1,9 @@
 #include "Base.hpp"
 
 Base::~Base() {}
+A::A(){}
+B::B(){}
+C::C(){}
 
 Base* generate(void)
 {
@@ -9,7 +12,7 @@ Base* generate(void)
 	switch((val % 3) + 1)
 	{
 		case 1:
-			return (new A())
+			return (new A());
 		case 2:
 			return (new B());
 		case 3:
@@ -20,6 +23,13 @@ Base* generate(void)
 
 void identify(Base* p)
 {
+	Base* ptr; // = dynamic_cast<A>(ptr);
+	if (ptr = dynamic_cast<A>(ptr)) //ptr != nullptr)
+		std::cout << "type A" << std::endl;
+	else if
+
+	if (dynamic_cast<A>(ptr))
+
 
 }
 
