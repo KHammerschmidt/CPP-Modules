e52e381586aeb4340ebf9303f9180b428b9e8581
Katharina Hammerschmidt <khammers@1-C-17.42heilbronn.de>
2022-08-23 13:06:22 +0200

    CPP07

--- -FINISHED-CPP07/ex02/main.cpp
+++ -FINISHED-CPP07/ex02/main.cpp
@@ -58,10 +58,8 @@ int main(void)
 	{
 		std::cout << std::endl << "F I R S T  T E S T: I N T  A R R A Y" << std::endl;
 		Array<int>d(10);
-		std::cout << "size: " << d.size() << std::endl;
 		for (size_t i = 0; i < 2; i++)
 			d[i] = i + 1;
-		std::cout << "Value: " << d[1] << std::endl;
 		Array<int>test(d);
 		std::cout << "size: " << d.size() << std::endl;
 		for(int i = 0; i < 10; i++)
@@ -69,20 +67,20 @@ int main(void)
 			d[i] = i;
 			std::cout << "Value: " << d[i] << std::endl;
 		}
-		
+
 		std::cout << std::endl << "S E C O N D  T E S T:  F L O A T  A R R A Y" << std::endl;
-    	Array<float>f(10);
+    	Array<float>f(5);
 		std::cout << "size: " << f.size() << std::endl;
-		for(float i = 0.0f; i < 10; i++)
+		for(float i = 0.0f; i < 5; i++)
 		{
 			f[i] = i+0.123f;
 			std::cout << "Value: " << f[i] << std::endl;
 		}
 
         std::cout << std::endl << "T H I R D  T E S T:  S T R I N G  A R R A Y" << std::endl;
-        Array<std::string>s(5);
+        Array<std::string>s(3);
         std::cout << "size: " << s.size() << std::endl;
-        for (size_t i = 0; i < 5; i++)
+        for (size_t i = 0; i < 3; i++)
         {
             s[i] = "A";
             std::cout << "Value: " << s[i] << std::endl;
